8ebbee43e8dac145b288d7d3659ca02778515bfe
Dmitry Gordeev <dmitrijg934@gmail.com>
2020-05-12 18:21:41 +0300

    Initial commit. Created basic WEB MVC application. Added all required starter dependencies.

---
+++ src/main/java/nn/dgordeev/stickytwits/Application.java
@@ -0,0 +1,11 @@
+package nn.dgordeev.stickytwits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
